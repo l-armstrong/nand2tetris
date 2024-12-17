741f02179fa72931625ea9ffe586cc8675ef22ee
l-armstrong <larms007@fiu.edu>
2024-12-17 12:42:56 -0500

    start defining the symbol class

--- projects/6/symboltable.py
+++ projects/6/symboltable.py
@@ -1,1 +1,15 @@
 """manages the symbol table"""
+
+class SymbolTable:
+    def __init__(self):
+        self.table = {}
+    
+    def add_symbol(self, symbol, address):
+        self.table[symbol] = address
+    
+    def get_address(self, symbol):
+        return self.table[symbol]
+    
+    def __contains__(self, symbol):
+        return True if symbol in self.table.keys() else False
+    
